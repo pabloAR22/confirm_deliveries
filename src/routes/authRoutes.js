96da9ed635a06254b9863594db53dad5231047a1
Pablo Aristizabal <radabeca23@gmail.com>
2024-12-28 13:33:49 -0500

    Cambios menores, implementación de middleware de erificacion token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/register', authController.registerUser);
+router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const usersRoutes = require('./userRoutes');
 const authRoutes = require('./authRoutes');
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
-    router.use('/users', usersRoutes);
     router.use('/auth', authRoutes);
+    router.use('/users', authMiddleware , usersRoutes);
 }
 
 module.exports = routerApi;
