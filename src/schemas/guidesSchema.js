bee0ccdfc37e239f8b2db9909e84884d1a4b8109
Pablo Aristizabal <radabeca23@gmail.com>
2025-01-11 11:50:43 -0500

    Correciones mensajes de validacion JOI

--- src/schemas/guidesSchema.js
+++ src/schemas/guidesSchema.js
@@ -2,57 +2,57 @@ const Joi = require('joi');
 
 const guideSchema = Joi.object({
     guia: Joi.string().required().min(5).max(10).messages({
-        "string.empty": "El campo 'guia' no puede estar vacío",
-        "any.required": "El campo 'guia' es obligatorio",
-        "string.min": "El campo 'guia' debe tener al menos 5 caracteres",
-        "string.max": "El campo 'guia' no puede tener más de 10 caracteres",
+        "string.empty": "The field 'guia' can't be empty",
+        "any.required": "The field 'guia' it is mandatory",
+        "string.min": "The field 'guia' must be at least 5 characters",
+        "string.max": "The field 'guia' cannot be more than 10 characters",
     }),
     ubicacion: Joi.string().required().messages({
-        "string.empty": "El campo 'ubicacion' no puede estar vacío",
-        "any.required": "El campo 'ubicacion' es obligatorio",
+        "string.empty": "The field 'ubicacion' can't be empty",
+        "any.required": "The field 'ubicacion' it is mandatory",
     }),
     ciudad_destino: Joi.string().required().messages({
-        "string.empty": "El campo 'ciudad_destino' no puede estar vacío",
-        "any.required": "El campo 'ciudad_destino' es obligatorio",
+        "string.empty": "The field 'ciudad_destino' can't be empty",
+        "any.required": "The field 'ciudad_destino' it is mandatory",
     }),
     departamento_destino: Joi.string().required().messages({
-        "string.empty": "El campo 'departamento_destino' no puede estar vacío",
-        "any.required": "El campo 'departamento_destino' es obligatorio",
+        "string.empty": "The field 'departamento_destino' can't be empty",
+        "any.required": "The field 'departamento_destino' it is mandatory",
     }),
     codigo_postal: Joi.string().required().length(6).messages({
-        "string.empty": "El campo 'codigo_postal' no puede estar vacío",
-        "any.required": "El campo 'codigo_postal' es obligatorio",
-        "string.length": "El campo 'codigo_postal' debe tener 6 caracteres"
+        "string.empty": "The field 'codigo_postal' can't be empty",
+        "any.required": "The field 'codigo_postal' it is mandatory",
+        "string.length": "The field 'codigo_postal' must be 6 characters"
     }),
     telefono_contacto: Joi.string().length(10).pattern(/^[0-9]+$/).required().messages({
-        "string.empty": "El campo 'telefono_contacto' no puede estar vacío",
-        "any.required": "El campo 'telefono_contacto' es obligatorio",
-        "string.length": "El campo 'telefono_contacto' debe tener 10 caracteres",
-        "string.pattern.base": "El campo 'telefono_contacto' solo puede contener números"
+        "string.empty": "The field 'telefono_contacto' can't be empty",
+        "any.required": "The field 'telefono_contacto' it is mandatory",
+        "string.length": "The field 'telefono_contacto' must be 10 characters",
+        "string.pattern.base": "The field 'telefono_contacto' can only contain numbers"
     }),
     valor_productos: Joi.number().min(1).required().messages({
-        "number.base": "El campo 'valor_productos' debe ser un número",
-        "any.required": "El campo 'valor_productos' es obligatorio",
-        "number.min": "El campo 'valor_productos' debe ser un número mayor que 0"
+        "number.base": "The field 'valor_productos' must be a number",
+        "any.required": "The field 'valor_productos' it is mandatory",
+        "number.min": "The field 'valor_productos' must be a number greater than 0"
     }),
     costo_envio: Joi.number().min(1).required().messages({
-        "number.base": "El campo 'costo_envio' debe ser un número",
-        "any.required": "El campo 'costo_envio' es obligatorio",
-        "number.min": "El campo 'costo_envio' debe ser un número mayor que 0"
+        "number.base": "The field 'costo_envio' must be a number",
+        "any.required": "The field 'costo_envio' it is mandatory",
+        "number.min": "The field 'costo_envio' must be a number greater than 0"
     }),
     observaciones: Joi.string().max(100).optional().messages({
-        "string.max": "El campo 'observaciones' no puede tener más de 100 caracteres",
+        "string.max": "The field 'observaciones' cannot be more than 100 characters",
     }),
     id_proveedor: Joi.number().required().messages({
-        "number.base": "El campo 'id_proveedor' debe ser un número",
-        "any.required": "El campo 'id_proveedor' es obligatorio",
+        "number.base": "The field 'id_proveedor' must be a number",
+        "any.required": "The field 'id_proveedor' it is mandatory",
     }),
     numero_cuenta: Joi.number().required().messages({
-        "number.base": "El campo 'numero_cuenta' debe ser un número",
-        "any.required": "El campo 'numero_cuenta' es obligatorio",
+        "number.base": "The field 'numero_cuenta' must be a number",
+        "any.required": "The field 'numero_cuenta' it is mandatory",
     }),
     linea_contacto: Joi.string().max(50).optional().messages({
-        "string.max": "El campo 'linea_contacto' no puede tener más de 50 caracteres",
+        "string.max": "The field 'linea_contacto' cannot be more than 50 characters",
     })
 });
 
